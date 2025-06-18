dae5e78bad8467585804983aa805322928f41526
Pieter <130872729+PieterJDSw@users.noreply.github.com>
2025-06-18 23:48:17 +0200

    Code Coverage Reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       coverage: {
         provider: 'istanbul',
-        reportsDirectory: './tests/unit/coverage',
+        reportsDirectory: './coverage',
         reporter: ['text', 'json', 'html'],
       },
       root: fileURLToPath(new URL('./', import.meta.url)),
